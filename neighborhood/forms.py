c775cd0ef5a5cd9a92f6d705fc07f64ddfba9115
weshy007 <josephwaweru96@gmail.com>
2021-09-25 18:16:45 +0300

    forms and registered admin

---
+++ neighborhood/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Profile,Neighborhood,Business,Post
+from django.contrib.auth.models import User
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('photo','name','bio')
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Input a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class NeighborHoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighborhood
+        exclude = ('admin','occupants_count')
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('neighborhood','user')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
