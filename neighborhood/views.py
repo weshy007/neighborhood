66ab70bdeab9937cd662182304ba470ff1ecf6ab
weshy007 <josephwaweru96@gmail.com>
2021-09-25 21:18:47 +0300

    single hood func and template

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Profile,Neighborhood,Business,Post
 from rest_framework.views import APIView
@@ -34,14 +34,53 @@ def hoods(request):
     pass
 
 @login_required(login_url='/accounts/login/')
-def single_hood(request):
-    pass
+def single_hood(request, hood_id):
+    hood = Neighborhood.objects.get(id=hood_id)
+    business = Business.objects.filter(neighborhood=hood)
+    posts = Post.objects.filter(hood=hood)
+    posts = posts[::-1]
+    if request.method == 'POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            bs_form = form.save(commit=False)
+            bs_form.neighborhood = hood
+            bs_form.user = request.user.profile
+            bs_form.save()
+            return redirect('single_hood', hood.id)
+    else:
+        form = BusinessForm()
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post = post_form.save(commit=False)
+            post.hood = hood
+            post.user = request.user.profile
+            post.save()
+            return redirect('single_hood', hood.id)
+    else:
+        post_form = PostForm()
+    parameters = {
+        'hood': hood,
+        'business': business,
+        'post_form': post_form,
+        'posts': posts,
+        'form': form,
+    }
+    return render(request, 'single_hood.html', parameters)
+
 
 def join(request):
-    pass
+    hood = get_object_or_404(Neighborhood, id=id)
+    request.user.profile.neighborhood = hood
+    request.user.profile.save()
+    return redirect('single_hood', hood.id)
+
 
 def leave(request):
-    pass
+    hood = get_object_or_404(Neighborhood, id=id)
+    request.user.profile.neighborhood = None
+    request.user.profile.save()
+    return redirect('hoods')
 
 @login_required(login_url='/accounts/login/')
 def search_profile(request):
