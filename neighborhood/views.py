b1281af5bb11e967403aca8b83da4141bae0302a
weshy007 <josephwaweru96@gmail.com>
2021-09-25 11:31:33 +0300

    views blueprints

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Profile,Neighborhood,Business,Post
 
 # Create your views here.
+def index(request):
+    pass
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    pass
+
+@login_required(login_url='/accounts/login/')
+def hoods(request):
+    pass
+
+@login_required(login_url='/accounts/login/')
+def single_hood(request):
+    pass
+
+def join(request):
+    pass
+
+def leave(request):
+    pass
+
+@login_required(login_url='/accounts/login/')
+def search_profile(request):
+    pass
